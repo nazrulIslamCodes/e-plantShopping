6fc9ae5da13a4b2323e4fad128fb59deff5f11e1
Nazrul Islam Ridwan <md.nazrul.islam.professional@gmail.com>
2024-10-07 12:20:19 -0700

    Update base path in vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/e-plantShopping/", // Set this to your GitHub repository name
   plugins: [react()],
 })
